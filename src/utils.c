6bbb2133d4158b22f98c7ca8986e9ef3c6b6ac7e
emartinez <enri.fc.98@gmail.com>
2023-03-04 11:30:34 +0100

    FIX: check if path or cmd given are null before joining them

--- src/utils.c
+++ src/utils.c
@@ -6,7 +6,7 @@
 /*   By: franmart <franmart@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/02 17:50:35 by franmart          #+#    #+#             */
-/*   Updated: 2023/01/23 20:06:48 by franmart         ###   ########.fr       */
+/*   Updated: 2023/03/04 11:27:31 by franmart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,9 @@ int	ft_open_file(char *filename, int mode)
 	int	fd;
 
 	if (mode == READ_MODE)
-		fd = open(filename, O_RDONLY);
+		fd = open(filename, O_RDONLY, 0777);
 	else if (mode == WRITE_MODE)
-		fd = open(filename, O_TRUNC | O_RDWR | O_CREAT, 0644);
+		fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0777);
 	else
 		fd = -1;
 	if (fd == -1)
@@ -30,10 +30,13 @@ int	ft_open_file(char *filename, int mode)
 char	*cmd_in_path(char *cmd, char *path)
 {
 	char	*file;
+	char	*check_path;
 
-	path = ft_strjoin("/", path);
-	file = ft_strjoin(cmd, path);
-	free(path);
+	if (!path || !cmd)
+		return (0);
+	check_path = ft_strjoin("/", path);
+	file = ft_strjoin(cmd, check_path);
+	free(check_path);
 	if (access(file, X_OK) == 0)
 		return (file);
 	free(file);
